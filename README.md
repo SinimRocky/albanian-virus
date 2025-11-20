a268cd2a43b18bd2fec37fa7373785c4092f7bab
simon.weibel <simon.weibel@ost.ch>
2025-11-20 19:56:52 +0100

    push shit

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ Compiling
 If you have [Zig][2] installed, you can just run this:
 
 ```sh
-zig cc albanian-virus.c -target x86_64-windows-gnu -o virus.exe
+clang albanian-virus.c -o virus.exe --target=x86_64-w64-mingw32 -mwindows -luser32 -lwinmm
 ```
 
 Other C compilers should also work, but I didn't bother testing them since `zig cc` is the only one that can easily cross-compile (I don't use Windows)
--- albanian-virus.c
+++ albanian-virus.c
@@ -1,14 +1,18 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
+#include <mmsystem.h>
+#pragma comment(lib, "winmm.lib")
 
 int main() {
 
-  MessageBox(
+  int r = MessageBox(
     NULL, 
     "Hi, I am an Albanian virus but because of poor technology in my country unfortunately I am not able to harm your computer. Please be so kind to delete one of your important files yourself and then forward me to other users. Many thanks for your cooperation! Best regards, Albanian virus.",
     "Virus Alert!",
     MB_ICONERROR | MB_YESNOCANCEL | MB_DEFBUTTON1
   );
+
+    PlaySound(TEXT("SystemHand"), NULL, SND_ALIAS | SND_ASYNC);
 
   return 0;
 }
