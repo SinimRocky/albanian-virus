4edf0192d6c498d374ce5167d915bf2854cbe77f
simon.weibel <simon.weibel@ost.ch>
2025-11-20 12:26:20 +0100

    spacing

--- albanian-virus.c
+++ albanian-virus.c
@@ -5,8 +5,8 @@ int main() {
 
   MessageBox(
     NULL, 
-    "Hi, I am an Albanian virus but because of poor technology in my country unfortunately I am not able to harm your computer. Please be so kind to delete one of your important files yourself and then forward me to other users. Many thanks for your cooperation! Best regards,Albanian virus",
-    "Virus Alert !",
+    "Hi, I am an Albanian virus but because of poor technology in my country unfortunately I am not able to harm your computer. Please be so kind to delete one of your important files yourself and then forward me to other users. Many thanks for your cooperation! Best regards, Albanian virus.",
+    "Virus Alert!",
     MB_ICONERROR | MB_YESNOCANCEL | MB_DEFBUTTON1
   );
 
